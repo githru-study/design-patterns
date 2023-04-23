e10e9b7e199391560e884ffe12dc6829bb5fa90d
조예진 <orobos654@gmail.com>
2023-04-23 17:42:26 +0900

    impl exportAsXml

--- behavioral/visitor/visitor.ts
+++ behavioral/visitor/visitor.ts
@@ -1,3 +1,4 @@
+import builder from "xmlbuilder";
 export abstract class Component {
     protected parent!: Component | null;
 
@@ -13,7 +14,8 @@ export abstract class Component {
     public abstract add(component: Component): void;
     public abstract remove(component: Component): void;
 
-    public abstract exportAsXml(): string;
+    public abstract getNodeName(): string;
+    public abstract exportAsXml(baseNode?: builder.XMLElement): string;
 }
 
 export class Page extends Component {
@@ -31,8 +33,17 @@ export class Page extends Component {
         this.children.delete(component);
     }
 
-    public exportAsXml(): string {
-        return "";
+    public getNodeName(): string {
+        return 'Page';
+    }
+
+    public exportAsXml(baseNode?: builder.XMLElement): string {
+        const result = (baseNode ?? builder.create(this.getNodeName()))
+        this.children.forEach(x => {
+            // x.constructor.name 등의 방법으로 접근 시, minify 과정 거쳤을 때 클래스 명이 다른 이름으로 바뀔 수 있음
+            x.exportAsXml(result.ele(x.getNodeName()))
+        })
+        return result.toString()
     }
 }
 
@@ -54,8 +65,17 @@ export class CompoundShape extends Component {
         this.children.delete(component);
     }
 
-    public exportAsXml(): string {
-        return "";
+    public getNodeName(): string {
+        return 'CompoundShape';
+    }
+
+    public exportAsXml(baseNode?: builder.XMLElement): string {
+        const result = (baseNode ?? builder.create(this.getNodeName()))
+        this.children.forEach(x => {
+            // x.constructor.name 등의 방법으로 접근 시, minify 과정 거쳤을 때 클래스 명이 다른 이름으로 바뀔 수 있음
+            x.exportAsXml(result.ele(x.getNodeName()))
+        })
+        return result.toString()
     }
 }
 
@@ -87,8 +107,17 @@ export class Circle extends Shape {
         super(center);
     }
 
-    public exportAsXml(): string {
-        return "";
+    public getNodeName(): string {
+        return 'Circle';
+    }
+
+    public exportAsXml(baseNode?: builder.XMLElement): string {
+        return (baseNode ?? builder.create(this.getNodeName()))
+            .att('centerX', this.position.x)
+            .att('centerY', this.position.y)
+            .att('radius', this.radius)
+            .att('area', Math.floor(Math.pow(this.radius, 2) * Math.PI))
+            .toString()
     }
 }
 
@@ -97,7 +126,19 @@ export class Rectangle extends Shape {
         super(lt);
     }
 
-    public exportAsXml(): string {
-        return "";
+    public getNodeName(): string {
+        return 'Rectangle';
+    }
+
+    public exportAsXml(baseNode?: builder.XMLElement): string {
+        return (baseNode ?? builder.create(this.getNodeName()))
+            .att('left', this.position.x)
+            .att('top', this.position.y)
+            .att('right', this.position.x + this.width)
+            .att('bottom', this.position.y + this.height)
+            .att('width', this.width)
+            .att('height', this.height)
+            .att('area', this.width * this.height)
+            .toString()
     }
 }
